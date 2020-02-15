642d4c3a2b0ff60aa413159a5fb7e44667596592
Sravan Kumar <sravan.kumar@fyle.in>
2020-02-16 01:54:13 +0530

    minor push

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['dev-helper-blog.herokuapp.com', 'localhost']
 
 # Application definition
 
